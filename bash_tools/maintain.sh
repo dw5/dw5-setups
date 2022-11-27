b2a1005b91c2b3947281aca24e3326c8513bfd11
dw5 <29355584+dw5@users.noreply.github.com>
2022-11-27 15:15:40 +0200

    Update maintain.sh

--- bash_tools/maintain.sh
+++ bash_tools/maintain.sh
@@ -2,20 +2,26 @@
 # linux
 mkdir recoded; for i in *.mp4; do ffmpeg -i "$i" "./recoded/${i%.*}-recoded.mp4"; done
 mkdir recoded; for i in *.m4a; do ffmpeg -i "$i" "${i%.*}-recoded.mp3"; done
-mkdir recoded; mogrify -format jpg -path ./recoded *.png
+mkdir recoded; mogrify -format jpg -path ./recoded *.png # turbo
+mkdir recoded; mogrify -format avif -path ./recoded *.png # do in bg, slow
 
 ` fdupes -SrdN . ` # dont ask just do it
 ` fdupes -Srd . ` # see all files before delete
 
 # windows
-mkdir recoded && mogrify -format jpg -path ./recoded *.png..
+mkdir recoded    && mogrify -format jpg -path ./recoded *.png
+mkdir recoded    && mogrify -format avif -path ./recoded *.png
 mkdir recodedvid && FOR %y IN (*.webm) DO @ffmpeg -i "%y" "./recodedvid/%y-recoded.mp4"
-FOR %y IN (*.mp4) DO @ffmpeg -i "%y" "%y-recoded.mp4"
+
 FOR %y IN (*.wav) DO @ffmpeg -i "%y" "%y-recoded.opus"
+FOR %y IN (*.opus) DO @ffmpeg -i "%y" "%y-recoded.mp3"
+FOR %y IN (*.mp4) DO @ffmpeg -i "%y" "%y-recoded.mp4"
 FOR %y IN (*.avi) DO @ffmpeg -i "%y" "%y-recoded.mp4"
+FOR %y IN (*.webm) DO @ffmpeg -i "%y" "%y-recoded.mp4"
 
 # tool goodies
 https://github.com/overcast07/wayback-machine-spn-scripts/blob/main/spn.sh
+-vf scale=3840:2160 # to 4K in order to better preserve <720p videos on video platforms
 
 wget -e robots=off
 spn.sh -p 1 tree.txt
