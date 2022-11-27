4188b203c17aed6b421337b0c53aa5a181a1d17a
dw5 <29355584+dw5@users.noreply.github.com>
2022-11-27 21:26:42 +0200

    archivist-renamer

---
+++ bash_tools/archivist-renamer.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# WIP f81337: purpose is to keep original file date and timestamps
+# after file conversion to modern and efficient formats
+# PRO TIP: hey if you can, use archivist if you can't change paths
+
+# === get file timestamps ===
+archivistlogfile="archivist.log"
+renametodolist="archvst-todo.sh" 
+# ^ so whatever it got changed to can be edited in text editor, you wont apply it to .wmv when there is .av1 ? :D
+rm $archivistlogfile
+rm $renametodolist
+
+for curFile in *; do
+if [ -f "${curFile}" ]; then #only FILES, no folders // -d directories -f files
+# $(date -d @<stat unix output> +%Y%m%d%H%M)
+    curFileAXtime=$(stat -c %X "$curFile") # access
+    curFileMDtime=$(stat -c %Y "$curFile") # modify
+    #echo "'$curFile' $curFileAXtime $curFileMDtime"
+    #echo "'$curFile'§$curFileAXtime§$curFileMDtime§">>$archivistlogfile
+
+if (( $curFileAXtime > $curFileMDtime )); then
+echo "$curFile $curFileAXtime is more than Modify $curFileMDtime"
+echo "mv '$curFile' '$(date -d @$curFileAXtime +%Y%m%d%H%M)-$curFile'">>$renametodolist
+else
+echo "$curFile $curFileMDtime is more than Access $curFileAXtime"
+echo "mv '$curFile' '$(date -d @$curFileMDtime +%Y%m%d%H%M)-$curFile'">>$renametodolist
+fi
+
+fi
+done
+exit 1
