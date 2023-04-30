9c635ddace5b6ef6b1bab21dcc31975b44ec3ccd
dw5 <29355584+dw5@users.noreply.github.com>
2023-04-30 22:21:28 +0300

    Create init.sh

---
+++ ffmpeg-hls-dash/init.sh
@@ -0,0 +1,12 @@
+todo
+apt install ffmpeg make cmake....
+
+
+git clone https://github.com/axiomatic-systems/Bento4.git
+cd Bento4/
+
+mkdir cmakebuild
+cd cmakebuild/
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
+make install
